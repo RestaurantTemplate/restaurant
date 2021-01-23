59fecee6dc404ef6b11fe0671fd192f07285e56f
mos.chutipon9 <mos.chutipon9@gmail.com>
2021-01-23 15:06:45 +0700

    add orders

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,14 @@ import App from './App'
 import Customer from './pages/Customer'
 import { Dashboard } from './pages/Dashboard'
 import NotFound from './pages/NotFound'
+import Orders from './staff/pages/Orders/Orders'
 
 const Routes = () => (
     <Switch>
         <Route exact path="/" component={App} />
         <Route path="/customer" component={Customer} />
         <Route path="/dashboard" component={Dashboard} />
+        <Route path="/orders" component={Orders} />
         <Route path="*" component={NotFound} />
     </Switch>
 )
