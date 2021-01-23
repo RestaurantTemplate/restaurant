eb597b69d803f9377c4c92b63cc6f0f79563ed69
salidwong wa <wansanohj@gmail.com>
2021-01-23 17:17:37 +0700

    [frontend] create header component

--- src/pages/Dashboard/components/index.js
+++ src/pages/Dashboard/components/index.js
@@ -1,7 +1,5 @@
-import { Title } from './Title'
-import { Location } from './Location'
 import { TextRecommend } from './TextRecommend'
 import { ItemList } from './ItemList'
 import { Item } from './Item'
 
-export { Title, Location, TextRecommend, ItemList, Item }
+export { TextRecommend, ItemList, Item }
