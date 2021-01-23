fa13542b7fe30be666968faa1ea1908f72263f8d
salidwong wa <wansanohj@gmail.com>
2021-01-23 15:19:20 +0700

    [frontend] create summary report component

--- src/pages/Dashboard/components/TextRecommend.js
+++ src/pages/Dashboard/components/TextRecommend.js
@@ -1,4 +1,4 @@
-import { Container, makeStyles } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core'
 import { Label } from '../../../components/Label'
 
 const useStyles = makeStyles({
@@ -11,10 +11,8 @@ export const TextRecommend = () => {
     const classes = useStyles()
 
     return (
-        <Container>
-            <Label variant="h4" className={classes.textRecommend}>
-                สินค้าขายดี
-            </Label>
-        </Container>
+        <Label variant="h4" className={classes.textRecommend}>
+            สินค้าขายดี
+        </Label>
     )
 }
